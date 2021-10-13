6dd2a56de1761be746f116951a3611c3b344920b
chryzsean <bryantforbes7@gmail.com>
2021-10-13 15:23:07 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using HtmlAgilityPack;
+using System.Linq;
+namespace CSharpHTMLReader
+
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument document = web.Load("https://en.wikipedia.org/wiki/HTML"); // View page source
+
+
+            var title = document.DocumentNode.SelectNodes("//*[@id=\"firstHeading\"]").First().InnerText; // scraping h1 inside div
+            var paragraphs = document.DocumentNode.SelectNodes("//*[@id=\"mw-content-text\"]/div[1]/p");
+            
+            Console.WriteLine(title);
+            Console.WriteLine();
+            //Console.WriteLine(paragraphs);
+            paragraphs.ToList().ForEach(i => Console.WriteLine(i.InnerText));
+
+
+        }
+    }
+}
